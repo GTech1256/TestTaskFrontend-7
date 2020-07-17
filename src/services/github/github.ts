79f99851f5fce8f69f9b5fe45433dff4e2db4dc8
GTech1256 <pomah156@gmail.com>
2020-07-17 10:09:19 +0300

    Добавляет сообщение об ошибке при отсутсвии .env.local

--- src/services/github/github.ts
+++ src/services/github/github.ts
@@ -1,8 +1,12 @@
+const TOKEN_MESSAGE_ERROR = 'REACT_APP_GITHUB_ACCESS_TOKEN not provide on .env'
+
+
 export async function getGithubToken(): Promise<string> {
   const token = process.env.REACT_APP_GITHUB_ACCESS_TOKEN
   
   if (!token) {
-    throw new Error('REACT_APP_GITHUB_ACCESS_TOKEN not provide on .env')
+    console.error(TOKEN_MESSAGE_ERROR)
+    throw new Error(TOKEN_MESSAGE_ERROR)
   }
 
   return token
